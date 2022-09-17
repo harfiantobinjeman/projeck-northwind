615bccf7ba5832eeb43f4b2b815382dc7a45af41
danieldewanto1906 <danieldewanto1906@gmail.com>
2022-09-17 15:27:15 +0700

    Add project files.

---
+++ Northwind.Domain/Base/IRepositoryManager.cs
@@ -0,0 +1,19 @@
+﻿using Northwind.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Base
+{
+    public interface IRepositoryManager
+    {
+        ICategoryRepository CategoryRepository { get; }
+        ICustomerRepository EmployeeRepository { get; }
+
+        void Save();
+
+        Task SaveAsync();
+    }
+}
