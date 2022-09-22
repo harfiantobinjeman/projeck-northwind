5a4c44e71fe19fc2ec80d6b4711e14ab8a7086ef
danieldewanto1906 <danieldewanto1906@gmail.com>
2022-09-22 14:47:05 +0700

    Done Day06

---
+++ Northwind.Test/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Northwind.Contracts.Dto.Category;
+using Northwind.Contracts.Dto.Product;
+using Northwind.Contracts.Dto.Supplier;
+using Northwind.Domain.Models;
+
+namespace Northwind.Web.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryForCreateDto>().ReverseMap();
+
+            CreateMap<Supplier, SupplierDto>().ReverseMap();
+            CreateMap<Supplier, SupplierForCreateDto>().ReverseMap();
+
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Product, ProductForCreateDto>().ReverseMap();
+        }
+    }
+}
---
+++ Northwind.Test/NorthwindIntegrationTest.cs
@@ -0,0 +1,140 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Northwind.Contracts.Dto.Category;
+using Northwind.Domain.Base;
+using Northwind.Domain.Models;
+using Northwind.Persistence;
+using Northwind.Persistence.Base;
+using Northwind.Services;
+using Northwind.Services.Abstraction;
+using Northwind.Web.Mapping;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Northwind.Test
+{
+    public class NorthwindIntegrationTest
+    {
+        private static IConfigurationRoot Configuration;
+        private static DbContextOptionsBuilder<NorthwindContext> optionBuilder;
+        private static MapperConfiguration mapperConfig;
+        private static IMapper mapper;
+        private static IServiceProvider serviceProvider;
+        private static IRepositoryManager _repositoryManager;
+
+        public NorthwindIntegrationTest()
+        {
+            BuildConfiguration();
+            SetupOptions();
+        }
+        [Fact]
+        public void TestCreateCategoryService()
+        {
+            using (var context = new NorthwindContext(optionBuilder.Options))
+            {
+                // act
+                _repositoryManager = new RepositoryManager(context);
+                IServiceManager serviceManager = new ServiceManager(_repositoryManager, mapper);
+                var categoryForCreate = new CategoryForCreateDto
+                {
+                    CategoryName = "Toys",
+                    Description = "Mainan Anak"
+                };
+                serviceManager.CategoryService.Insert(categoryForCreate);
+
+                //assert
+                var category = serviceManager.CategoryService.GetAllCategory(false);
+                category.ShouldNotBeNull();
+                category.Result.Count().ShouldBe(13);
+            }
+        }
+
+        [Fact]
+        public void TestGetCategoryService()
+        {
+            using(var context = new NorthwindContext(optionBuilder.Options))
+            {
+                _repositoryManager = new RepositoryManager(context);
+                IServiceManager serviceManager = new ServiceManager(_repositoryManager, mapper);
+                var categoryService = serviceManager.CategoryService.GetAllCategory(false);
+                categoryService.ShouldNotBeNull();
+                categoryService.Result.Count().ShouldBe(12);
+            }
+        }
+        [Fact]
+        public void TestCreateCategoryRepo()
+        {
+            using (var context = new NorthwindContext(optionBuilder.Options))
+            {
+                // act
+                _repositoryManager = new RepositoryManager(context);
+
+                //define model category
+                var categoryModel = new Category
+                {
+                    CategoryName = "Bag",
+                    Description = "Eiger"
+                };
+                _repositoryManager.CategoryRepository.Insert(categoryModel);
+                _repositoryManager.Save();
+                categoryModel.CategoryId.ShouldBeEquivalentTo(23);
+
+                //assert
+                var category = _repositoryManager.CategoryRepository.GetAllCategory(false);
+                category.ShouldNotBeNull();
+                category.Result.Count().ShouldBe(12);
+                
+
+            }
+        }
+
+        [Fact]
+        public void TestGetCategoryRepo()
+        {
+            using (var context = new NorthwindContext(optionBuilder.Options))
+            {
+                // act
+                _repositoryManager = new RepositoryManager(context);
+                var category = _repositoryManager.CategoryRepository.GetAllCategory(false);
+
+                //assert
+                category.ShouldNotBeNull();
+                category.Result.Count().ShouldBe(10);
+            }
+        }
+
+        private void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        private void SetupOptions()
+        {
+            optionBuilder = new DbContextOptionsBuilder<NorthwindContext>();
+            optionBuilder.UseSqlServer(Configuration.GetConnectionString("NorthwindDb"));
+
+            var service = new ServiceCollection();
+            service.AddAutoMapper(typeof(MappingProfile));
+            serviceProvider = service.BuildServiceProvider();
+
+            mapperConfig = new MapperConfiguration(cfg => 
+            { 
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            mapperConfig.AssertConfigurationIsValid();
+            mapper = mapperConfig.CreateMapper();
+        }
+    }
+}
